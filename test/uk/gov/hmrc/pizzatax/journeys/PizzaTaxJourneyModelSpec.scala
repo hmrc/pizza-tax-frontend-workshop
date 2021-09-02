d44b1424e38c4b382da53d81efd6b3113d82b992
Artur Opala <250927+arturopala@users.noreply.github.com>
2021-09-02 08:29:11 +0100

    update tests

--- test/uk/gov/hmrc/pizzatax/journeys/PizzaTaxJourneyModelSpec.scala
+++ test/uk/gov/hmrc/pizzatax/journeys/PizzaTaxJourneyModelSpec.scala
@@ -28,50 +28,50 @@ class PizzaTaxJourneyModelSpec extends AnyWordSpec with Matchers with JourneyMod
 
   import model.{State, Transitions}
 
+  val questionnaireStep01 =
+    QuestionnaireAnswers(haveYouBeenHungryRecently = Some(true))
+
   "PizzaTaxJourneyModel" when {
 
     "at state Start" should {
-      "goto HaveYouBeenHungryRecently when start" in {
+      "goto HaveYouBeenHungryRecently when start" in
         given(State.Start)
           .when(Transitions.start)
           .thenGoes(State.HaveYouBeenHungryRecently(QuestionnaireAnswers.empty))
-      }
-      "goto Start when askHaveYouBeenHungryRecently" in {
+
+      "goto Start when askHaveYouBeenHungryRecently" in
         given(State.Start)
           .when(Transitions.askHaveYouBeenHungryRecently)
           .thenGoes(State.Start)
-      }
-      "do nothing when submittedHaveYouBeenHungryRecently=true" in {
+
+      "do nothing when submittedHaveYouBeenHungryRecently=true" in
         given(State.Start)
           .when(Transitions.submittedHaveYouBeenHungryRecently(true))
           .thenNoChange
-      }
+
     }
 
     "at state HaveYouBeenHungryRecently" should {
-      "goto an empty HaveYouBeenHungryRecently when start" in {
-        given(State.HaveYouBeenHungryRecently(QuestionnaireAnswers(haveYouBeenHungryRecently = Some(true))))
+      "goto an empty HaveYouBeenHungryRecently when start" in
+        given(State.HaveYouBeenHungryRecently(questionnaireStep01))
           .when(Transitions.start)
           .thenGoes(State.HaveYouBeenHungryRecently(QuestionnaireAnswers.empty))
-      }
 
-      "stay when askHaveYouBeenHungryRecently" in {
-        given(State.HaveYouBeenHungryRecently(QuestionnaireAnswers(haveYouBeenHungryRecently = Some(true))))
+      "stay when askHaveYouBeenHungryRecently" in
+        given(State.HaveYouBeenHungryRecently(questionnaireStep01))
           .when(Transitions.askHaveYouBeenHungryRecently)
           .thenNoChange
-      }
 
-      "goto WorkInProgressDeadEnd when submittedHaveYouBeenHungryRecently=true" in {
+      "goto WorkInProgressDeadEnd when submittedHaveYouBeenHungryRecently=true" in
         given(State.HaveYouBeenHungryRecently(QuestionnaireAnswers.empty))
           .when(Transitions.submittedHaveYouBeenHungryRecently(true))
           .thenGoes(State.WorkInProgressDeadEnd)
-      }
 
-      "goto WorkInProgressDeadEnd when submittedHaveYouBeenHungryRecently=false" in {
+      "goto WorkInProgressDeadEnd when submittedHaveYouBeenHungryRecently=false" in
         given(State.HaveYouBeenHungryRecently(QuestionnaireAnswers.empty))
           .when(Transitions.submittedHaveYouBeenHungryRecently(false))
           .thenGoes(State.WorkInProgressDeadEnd)
-      }
+
     }
   }
 }
